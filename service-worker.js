493a1c0e795d6e3b49ebbffa5c7b76a4de0f9ee7
Veez20098 <virjeshsinghmar@gmail.com>
2025-09-16 22:28:17 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,36 @@
+const CACHE = "focusforge-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./apple-touch-icon.png",
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  const url = new URL(e.request.url);
+  // Network-first for non-GET or external; cache-first for same-origin GET
+  if (e.request.method !== "GET" || url.origin !== location.origin) return;
+  e.respondWith(
+    caches.match(e.request).then((cached) => cached || fetch(e.request).then((res) => {
+      const copy = res.clone();
+      caches.open(CACHE).then((cache) => cache.put(e.request, copy));
+      return res;
+    }).catch(() => cached))
+  );
+});
